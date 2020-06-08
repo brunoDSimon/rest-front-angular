c9bba4e0a218fa8691fed8a4b34700a5d09f0d50
bruno <brunosimon999@gmail.com>
2020-06-08 12:12:02 -0300

    response

--- src/app/features/dashboard/componets/sum-group-month-companies/sum-group-month-companies.component.ts
+++ src/app/features/dashboard/componets/sum-group-month-companies/sum-group-month-companies.component.ts
@@ -13,7 +13,8 @@ export class SumGroupMonthCompaniesComponent implements OnInit {
   private _monthsChart: any[] = [];
   private _conteudoChart: any[] = [];
   private response: any[] = [];
-  private _format: [];
+  private _periodos:any = [];
+  private _valor: any[]= []
   private _cores = [
     "#3e95cd",
     "#4c32a8",
@@ -21,6 +22,93 @@ export class SumGroupMonthCompaniesComponent implements OnInit {
     "#0B4C5F",
     "#8000FF",
   ];
+  private dados:any =[
+    {
+      "status": {
+        "value": "0",
+        "messege": "requisição efetuada com sucesso"
+      },
+      "data": [
+        {
+          "resultGroup": 3.72,
+          "periodo": "2020-01",
+          "companies": {
+            "companyName": "Sagga"
+          }
+        },
+        {
+          "resultGroup": 3446.0399999999963,
+          "periodo": "2020-03",
+          "companies": {
+            "companyName": "Arrezo"
+          }
+        },
+        {
+          "resultGroup": 40.92,
+          "periodo": "2020-03",
+          "companies": {
+            "companyName": "MCM"
+          }
+        },
+        {
+          "resultGroup": 13843.73,
+          "periodo": "2020-04",
+          "companies": {
+            "companyName": "Arrezo"
+          }
+        },
+        {
+          "resultGroup": 76.67999999999998,
+          "periodo": "2020-04",
+          "companies": {
+            "companyName": "Sagga"
+          }
+        },
+        {
+          "resultGroup": 148.26,
+          "periodo": "2020-05",
+          "companies": {
+            "companyName": "Sagga"
+          }
+        },
+        {
+          "resultGroup": 3.72,
+          "periodo": "2020-06",
+          "companies": {
+            "companyName": "Sagga"
+          }
+        },
+        {
+          "resultGroup": 3.72,
+          "periodo": "2020-07",
+          "companies": {
+            "companyName": "Sagga"
+          }
+        },
+        {
+          "resultGroup": 11.16,
+          "periodo": "2020-08",
+          "companies": {
+            "companyName": "Sagga"
+          }
+        },
+        {
+          "resultGroup": 100.43999999999998,
+          "periodo": "2020-08",
+          "companies": {
+            "companyName": "MCM"
+          }
+        },
+        {
+          "resultGroup": 29.759999999999994,
+          "periodo": "2020-12",
+          "companies": {
+            "companyName": "MCM"
+          }
+        }
+      ]
+    }
+  ]
   constructor(
     private dashboardService: DashboardService,
     private reais:ReaisPipe
@@ -38,9 +126,12 @@ export class SumGroupMonthCompaniesComponent implements OnInit {
   get monthsChart() {
     return this._monthsChart;
   }
-  get format(){
-    return this._format;
-  }
+ get periodo(){
+   return this._periodos
+ }
+ get valor(){
+   return this._valor
+ }
   public getSumGroupMonthCompanies(){
     this.dashboardService.sumGroupMonthCompanies().subscribe((res) =>{
       this.response = res.data;
@@ -51,15 +142,18 @@ export class SumGroupMonthCompaniesComponent implements OnInit {
           const index = this._conteudoChart.findIndex((t)=>t.periodo == res.periodo)
           if(index != -1){
             this._conteudoChart[index].data.push(res.resultGroup);
+            this._conteudoChart[index].label.push(res.companies.companyName);
           } else {
-            this._conteudoChart.push({periodo: res.periodo, name:res.companies.companyName, data: [res.resultGroup],backgroundColor:  this._cores[this._conteudoChart.length]});
+            this._conteudoChart.push({periodo:res.periodo, label: [res.companies.companyName], data: [res.resultGroup]})
           }
         } else {
-          this._conteudoChart.push({periodo: res.periodo, name:res.companies.companyName, data: [res.resultGroup],backgroundColor: this._cores[0]});
+          this._conteudoChart.push({periodo:res.periodo,label: [res.companies.companyName], data: [res.resultGroup]});
         }
       }); 
-      this.render()
       console.log(this._conteudoChart)
+      this._conteudoChart.map((aux) =>{this._valor.push(aux.data) })
+      console.log(this._valor)
+      this.render()
     },(error) =>{
       console.log(error)
     })
@@ -67,37 +161,47 @@ export class SumGroupMonthCompaniesComponent implements OnInit {
 
   public render(){
     this.myChart =new Chart(document.getElementById("bar-chart-grouped"), {
-      type: 'bar',
-      data: {
-        labels: this.monthsChart,
-        datasets: this.conteudoChart
-      },
+        type: 'bar',
+        // data: {
+        //   labels: this.monthsChart,
+        //   datasets: this.conteudoChart
+        // },
+        data: {
+          labels: this._monthsChart,
+          datasets: [
+            {
+              label: 'Soma total de ganhos por mes',
+              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+              data: this.valor
+            }
+          ]
+        },
       options: {
         title: {
           display: false,
           text: 'Ganhos agrupado por mes de cada compania'
         },
-        tooltips: {
-          backgroundColor: '#fff',
-          borderWidth: 1,
-          borderColor: '#999',
-          displayColors: false,
-          mode: 'point',
-          titleFontColor: 'black',
-          callbacks: {
-            title: () => {
-              return '';
-            },
-            label: (t, c) => {
-              // const label = c.datasets[t.datasetIndex].label[t.index];
-              const valor = this.reais.transform(c.datasets[t.datasetIndex].data[t.index]);
-              return `  ${valor}  `;
-            },
-            labelTextColor: (tooltipItem, chart) => {
-              return 'black';
-            }
-          }
-        },
+        // tooltips: {
+        //   backgroundColor: '#fff',
+        //   borderWidth: 1,
+        //   borderColor: '#999',
+        //   displayColors: false,
+        //   mode: 'point',
+        //   titleFontColor: 'black',
+        //   callbacks: {
+        //     title: () => {
+        //       return '';
+        //     },
+        //     label: (t, c) => {
+        //       // const label = c.datasets[t.datasetIndex].label[t.index];
+        //       const valor = this.reais.transform(c.datasets[t.datasetIndex].data[t.index]);
+        //       return `  ${valor}  `;
+        //     },
+        //     labelTextColor: (tooltipItem, chart) => {
+        //       return 'black';
+        //     }
+        //   }
+        // },
       },
       
    });
