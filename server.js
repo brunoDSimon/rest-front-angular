d2fd6ea9db80104de929fcc002e03d590bcee553
Bruno <brunosimon999@gmail.com>
2020-07-22 19:06:52 -0300

    finish angular json

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/project-angular'));
+app.use(express.static('./dist/projectAngular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/project-angular/'}),
+    res.sendFile('index.html', {root: 'dist/projectAngular/'}),
 );
 
 app.listen(process.env.PORT || 8080);
