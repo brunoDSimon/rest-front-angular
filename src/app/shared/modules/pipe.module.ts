1b7f0e83dc840b4ca2810803bb6fe1d9e60a7fbc
Bruno <brunosimon999@gmail.com>
2021-04-19 20:23:06 -0300

    melhorias em porcentagem

--- src/app/shared/modules/pipe.module.ts
+++ src/app/shared/modules/pipe.module.ts
@@ -3,21 +3,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MesAnoPipe } from '../pipe/mes-ano.pipe';
 import { CpfCnpjValidatorDirective } from '../directive/CpfCnpjValidatorDirective.directive';
+import { PercentageMaskDirective } from '../directive/percentage-mask.directive';
 
 
 @NgModule({
   declarations: [
     ReaisPipe,
     MesAnoPipe,
-      CpfCnpjValidatorDirective
+    CpfCnpjValidatorDirective,
+    PercentageMaskDirective
    ],
   providers:[
     ReaisPipe,
-    MesAnoPipe
+    MesAnoPipe,
+    CpfCnpjValidatorDirective,
+    PercentageMaskDirective
   ],
   exports:[
     ReaisPipe,
-    MesAnoPipe
+    MesAnoPipe,
+    CpfCnpjValidatorDirective,
+    PercentageMaskDirective
   ]
 })
 export class PipeModule { }
